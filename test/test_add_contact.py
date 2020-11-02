0fb632daa75f2211392b2fc8dc4cbcda46cc193a
Nataliya Yuryeva <yuryevan@gmail.com>
2020-11-02 11:41:20 +0100

    Class SessionHelper created

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     contact_data = Contact(firstname="Harry", middlename="J", lastname="Potter", nickname="magic", title="student",
                           company="Dumbledore", address="Privet Drive 4", home="2244611", mobile="226646", work="464651",
                           fax="515151", email="harry@magic.com", email2="ron@magic.com", email3="hermione@nkjn.com",
@@ -21,17 +21,17 @@ def test_add_contact(app):
                           amonth="January", ayear="1100", address2="Diagon Alley", phone2="101010", notes="969696")
     app.create_contact(contact_data)
     app.return_to_homepage()
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     contact_data = Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="",
                                home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="",
                                bday="", bmonth="-", byear="", aday="", amonth="-", ayear="", address2="", phone2="",
                                notes="")
     app.create_contact(contact_data)
     app.return_to_homepage()
-    app.logout()
+    app.session.logout()
 
