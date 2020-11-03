64350cf0779cd82f9708e74e9bb73828b0bfa774
Nataliya Yuryeva <yuryevan@gmail.com>
2020-11-03 13:52:33 +0100

    Insignificant format issues resolved

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,10 +6,11 @@ def test_add_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
     contact_data = Contact(firstname="Harry", middlename="J", lastname="Potter", nickname="magic", title="student",
-                          company="Dumbledore", address="Privet Drive 4", home="2244611", mobile="226646", work="464651",
-                          fax="515151", email="harry@magic.com", email2="ron@magic.com", email3="hermione@nkjn.com",
-                          homepage="www.hogwarts.com", bday="31", bmonth="July", byear="1980", aday="1",
-                          amonth="January", ayear="1100", address2="Diagon Alley", phone2="101010", notes="969696")
+                           company="Dumbledore", address="Privet Drive 4", home="2244611", mobile="226646",
+                           work="464651", fax="515151", email="harry@magic.com", email2="ron@magic.com",
+                           email3="hermione@nkjn.com", homepage="www.hogwarts.com", bday="31", bmonth="July",
+                           byear="1980", aday="1", amonth="January", ayear="1100", address2="Diagon Alley",
+                           phone2="101010", notes="969696")
     app.contact.create(contact_data)
     app.contact.return_to_homepage()
     app.session.logout()
@@ -19,9 +20,8 @@ def test_add_empty_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
     contact_data = Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="",
-                               home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="",
-                               bday="", bmonth="-", byear="", aday="", amonth="-", ayear="", address2="", phone2="",
-                               notes="")
+                           home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", bday="",
+                           bmonth="-", byear="", aday="", amonth="-", ayear="", address2="", phone2="", notes="")
     app.contact.create(contact_data)
     app.contact.return_to_homepage()
     app.session.logout()
