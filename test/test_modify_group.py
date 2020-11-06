e5966ed41149efab87b83e53df57f07ae4ee52df
Nataliya Yuryeva <yuryevan@gmail.com>
2020-11-06 11:12:06 +0100

    Group model, service methods and tests optimized to avoid mandatory parameters specifying

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,9 +2,13 @@
 from model.group import Group
 
 
-def test_modify_first_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="group50"))
     app.session.logout()
 
 
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="header50"))
+    app.session.logout()
