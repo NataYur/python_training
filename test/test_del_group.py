76312b8bc559fe074c0fcc31c2407f0cecff50e4
Nataliya Yuryeva <yuryevan@gmail.com>
2020-11-08 18:33:51 +0100

    GroupHelper updated to optimize the navigation

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,5 +3,5 @@ from model.group import Group
 
 def test_delete_first_group(app):
     if app.group.count() == 0:
-        app.group.create(Group(name='to delete'))
+        app.group.create(Group(name='to_delete'))
     app.group.delete_first_group()
