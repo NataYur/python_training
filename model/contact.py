8027a9e74f94ec27b92edee38864f1cbb1cccb81
Nataliya Yuryeva <yuryevan@gmail.com>
2020-11-24 13:46:13 +0100

    Test to verify a random contact test data added, ContactHelper updated

--- model/contact.py
+++ model/contact.py
@@ -6,7 +6,8 @@ class Contact:
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None,
                  address=None, homephone=None, mobilephone=None, workphone=None, faxphone=None, email=None, email2=None,
                  email3=None, homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None,
-                 address2=None, secondaryphone=None, notes=None, id=None, all_phones_from_homepage=None):
+                 address2=None, secondaryphone=None, notes=None, id=None, all_phones_from_homepage=None,
+                 all_emails_from_homepage=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -33,6 +34,7 @@ class Contact:
         self.notes = notes
         self.id = id
         self.all_phones_from_homepage = all_phones_from_homepage
+        self.all_emails_from_homepage = all_emails_from_homepage
 
     def __repr__(self):
         return"%s:%s:%s" % (self.id, self.firstname, self.lastname)
---
+++ test/test_random_contact_data.py
@@ -0,0 +1,36 @@
+import re
+from random import randrange
+from model.contact import Contact
+
+
+def test_contact_data_on_homepage(app):
+    contact_data = Contact(firstname="firstname", lastname="lastname", mobilephone="+31889977", workphone="(06)44100",
+                           email="email1@email.com", email3="email3@email.com", secondaryphone="45-45-45")
+    for i in range(0, 4):
+        app.contact.create(contact_data)
+    index = randrange(len(app.contact.get_contact_list()))
+    contact_data_from_homepage = app.contact.get_contact_list()[index]
+    contact_data_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_data_from_homepage.firstname == contact_data_from_edit_page.firstname
+    assert contact_data_from_homepage.lastname == contact_data_from_edit_page.lastname
+    assert contact_data_from_homepage.address == contact_data_from_edit_page.address
+    assert contact_data_from_homepage.all_phones_from_homepage == \
+           merge_phones_like_on_homepage(contact_data_from_edit_page)
+    assert contact_data_from_homepage.all_emails_from_homepage == \
+           merge_emails_like_on_homepage(contact_data_from_edit_page)
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+
+def merge_phones_like_on_homepage(contact):
+    return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter(lambda x: x is not None,
+                                                                              [contact.homephone, contact.mobilephone,
+                                                                               contact.workphone,
+                                                                               contact.secondaryphone]))))
+
+
+def merge_emails_like_on_homepage(contact):
+    return "\n".join(filter(lambda x: x != "", filter(lambda x: x is not None, [contact.email, contact.email2,
+                                                                                contact.email3])))
