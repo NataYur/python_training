1a1e3d9ef524f681ba1c3ac645b6b28c6e9b7e13
Nataliya Yuryeva <yuryevan@gmail.com>
2020-11-25 12:07:22 +0100

    Parametrizing test add contact implemented

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,31 +1,48 @@
 # -*- coding: utf-8 -*-
+import random
+import string
+import pytest
+import calendar
 from model.contact import Contact
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_string_year():
+    return str(random.randrange(1900, 2021))
+
+
+def random_string_month():
+    return calendar.month_name[random.randrange(1, 13)]
+
+
+def random_string_day():
+    return str(random.randrange(1, 32))
+
+
+testdata = [Contact(firstname="", lastname="")] + [
+    Contact(firstname=random_string("firstname", 15), middlename=random_string("middlename", 10),
+            lastname=random_string("lastname", 20), nickname=random_string("nickname", 20),
+            title=random_string("title", 15), company=random_string("company", 15), address=random_string("address", 10),
+            homephone="2244611", mobilephone="226646", workphone="464651", faxphone="515151", email="harry@magic.com",
+            email2="ron@magic.com", email3="hermione@nkjn.com", homepage="www.hogwarts.com", bday=random_string_day(),
+            bmonth=random_string_month(), byear=random_string_year(), aday=random_string_day(),
+            amonth=random_string_month(), ayear=random_string_year(), address2=random_string("address2", 10),
+            secondaryphone="101010", notes=random_string("notes", 10))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     app.open_home_page()
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Harry", middlename="J", lastname="Potter", nickname="magic", title="student",
-                      company="Dumbledore", address="Privet Drive 4", homephone="2244611", mobilephone="226646",
-                      workphone="464651", faxphone="515151", email="harry@magic.com", email2="ron@magic.com",
-                      email3="hermione@nkjn.com", homepage="www.hogwarts.com", bday="31", bmonth="July",
-                      byear="1980", aday="1", amonth="January", ayear="1100", address2="Diagon Alley",
-                      secondaryphone="101010", notes="969696")
     app.contact.create(contact)
     app.contact.return_to_homepage()
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-# def test_add_empty_contact(app):
-#     app.open_home_page()
-#     old_contacts = app.contact.get_contact_list()
-#     contact = Contact(firstname="", lastname="")
-#     app.contact.create(contact)
-#     app.contact.return_to_homepage()
-#     new_contacts = app.contact.get_contact_list()
-#     old_contacts.append(contact)
-#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from sys import maxsize
-
 import pytest
 from model.group import Group
 import random
